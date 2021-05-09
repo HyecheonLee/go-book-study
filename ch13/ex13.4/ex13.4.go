3423b8a967bf48cdf372dd0c2c250026499b9806
이혜천 <rainbow880616@gmail.com>
2021-05-10 01:50:48 +0900

    13장 완성.

---
+++ ch13/ex13.4/ex13.4.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Name  string
+	ID    string
+	Age   int
+	Level int
+}
+
+type VIPUser struct {
+	User
+	Level int
+	Price int
+}
+
+func main() {
+	user := User{"송하나", "hana", 23, 10}
+	vip := VIPUser{User{"화랑", "hwarang", 40, 10}, 3, 250}
+
+	fmt.Printf("유저: %s ID:%s 나이: %d\n", user.Name, user.ID, user.Age)
+	fmt.Printf("VIP 유저: %s ID: %s 나이: %d VIP 레벨: %d VIP 가격: %d 원 유저 레벨:%d\n",
+		vip.Name, vip.ID, vip.Age, vip.Level, vip.Price, vip.User.Level)
+
+}
